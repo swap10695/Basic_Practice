6b515407b85782fafb3a297bf54622418ea7d27e
Swapnil Borhade <Swap10695@gmail.com>
2023-03-06 16:05:20 +0530

    added implementation for FibonasiSeries.java

--- FibonasiSeries.java
+++ FibonasiSeries.java
@@ -1,3 +1,15 @@
 public class FibonasiSeries{
-    
+    public static void main(String[] args) {
+        int num =7;
+        int a=0;
+        int b=1;
+        int c;
+
+        for (int i=0; i<num; i++){
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+        }
+    }
 }
